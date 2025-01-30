101a309323ee621be2abd7e2da1a2971a192dfa7
Leoci Melo <leocij@hotmail.com>
2025-01-29 21:00:35 -0300

    feature/15-navbar-bootswatch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// yarn add bootswatch
+import 'bootswatch/dist/minty/bootstrap.min.css';
 import AppRoutes from './AppRoutes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
